ca8b83a635ae710daf54390f59c3ef570fbf6a82
alopezo <8TPFdVPnehjv4d>
2022-07-05 16:31:06 -0300

    App update

--- src/app/roadmap-content/current-states.ts
+++ src/app/roadmap-content/current-states.ts
@@ -1,10 +1,21 @@
 export const currentStates = [
     {
-      opSelector: "Add placeholder",
-      text: `<mark>[PLACEHOLDER]<br>
-      List topics for consideration, including: Size of healthcare sector (e.g. number of public/private facilities, healthcare providers), 
-      how the healthcare sector is broken up into regions, state of computerisation in each sector, existing standards used (e.g. messaging, information models, 
-      code systems), Local vendor environment, national infrastructure in place (or planned) such as connectivity, National EHR, expected challenges and barriers, etc.<br>
-      [PLACEHOLDER]</mark>`
+      opSelector: "Customized text",
+      text: `<mark>
+      <p>[INSTRUCTIONS - Please customize the following sample text, or replace it with your own]</p>
+      <p>The current eHealth landscape in <span class='country'>COUNTRY</span> can be summarized as follows:</p>
+      <ul>
+        <li>[N] healthcare regions, each responsible for [add regional healthcare responsibilities]</li>
+        <li>[N] hospitals ([N] public /[N] private), [N] general practice clinics, and [add other information to indicate healthcare organisation landscape]</li>
+        <li>[N] secondary care doctors, [N] general practitioners, and [add other information to indicate healthcare provider landscape]</li>
+        <li>[P]% of hospitals, [P]% of general practice clinics and [add other relevant healthcare organisations] currently have computerised clinical records</li>
+        <li>Vendors used include [Add text to describe the vendor landscape in each sector, or describe how clinical systems are locally developed]</li>
+        <li>The following eHealth standards are used: [add text to describe standards, e.g. HL7 (for messaging), National Minimum Data Set (for common information model)]</li>
+        <li>The following code systems are used: [add text to describe the terminologies and classifications currently used, where they are used, and for what purpose]</li>
+        <li>National infrastructure in place includes: [add text to describe any national infrastructure is place, or planned, such as connectivity, national EHR etc]</li>
+        <li>National legislation in place includes [add text to describe any relevant national or regional legislation in place that may be relevant]</li>
+        <li>Current challenges in  the eHealth sector include: [add text to describe any relevant challenges or barriers to achieving vision above]</li>
+      </ul>
+      <p>[END INSTRUCTIONS]</p></mark>`
     }
 ];
--- src/app/roadmap-content/projects.ts
+++ src/app/roadmap-content/projects.ts
@@ -1,9 +1,17 @@
 export const projects = [
     {
-        opSelector: "Placeholder",
-        text: `<mark>[PLACEHOLDER]<br>
-        Define use cases and location/scope of initial implementation focus. e.g. Hospital X, Clinic Y,
-        Clinical register Z, Region W, National EHR, National HIE infrastructure etc<br>
-        [PLACEHOLDER]</mark>`
+        opSelector: "Customized text",
+        text: `<mark>
+        <p>[INSTRUCTIONS - Please customize the following sample text, or replace it with your own]</P>
+        <p>To deliver on our eHealth strategy for <span class='country'>COUNTRY</span>, SNOMED CT will be implemented 
+        in the following eHealth projects: </p>
+        <ul>
+            <li>[ADD project name(s), use case(s), location(s) and scope(s) of initial implementation focus, e.g. 
+                emergency department system upgrade at hospital X]</li>
+            <li>[ADD national projects if relevant, e.g. national EHR, national clinical registry, national HIE 
+                infrastructure]</li>
+        </ul>
+        <p>[END INSTRUCTIONS]</p>
+        </mark>`
     }
 ];
