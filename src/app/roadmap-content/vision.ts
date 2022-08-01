ba5303a3fb478536ababc3f907ff2b9320d3065e
alopezo <8TPFdVPnehjv4d>
2022-08-01 11:20:08 -0300

    Update with member's priorities

--- src/app/roadmap-content/vision.ts
+++ src/app/roadmap-content/vision.ts
@@ -1,20 +1,38 @@
 export const visionIntro = `Our vision for SNOMED CT in <span class='country'>COUNTRY</span> is that:`;
 export const visions = [
     {
-    opSelector: "Better health",
-    text: `<li>The use of a unified clinical terminology in health information systems across <span class='country'>COUNTRY</span>
-    will result in better health and improved patient outcomes for all citizens.</li>`
+      opSelector: "Better health",
+      text: `<li>The use of a unified clinical terminology in health information systems across <span class='country'>COUNTRY</span>
+      will result in better health, improved patient outcomes, and a stronger engagement of all citizens to make informed health related choices.</li>`,
+      membersPriorities: [
+        'Improved health outcomes',
+        'Enhanced patient experience and outcome - personalised medicine through decision support systems',
+        'Greater transparency of the quality of care & enhanced practitioner accountability',
+        'Citizens as service users - empower & engage people to make informed health related choices'
+      ]
     },
     {
-    opSelector: "Continuity of care",
-    text: `<li>Leveraging the power of standardized clinical terminology in digital technologies will allow us to realize our goals of
-      strengthening the continuity of care services across <span class='country'>COUNTRY</span> and supporting the key principles 
-      of patient centricity and better access to health care for all.</li>`
+      opSelector: "Continuity of care",
+      text: `<li>Leveraging the power of standardized clinical terminology in digital technologies will allow us to realize our goals of
+        strengthening the continuity of care services across <span class='country'>COUNTRY</span> and supporting the key principles 
+        of patient centricity and better access to health care for all.</li>`,
+      membersPriorities: [
+        'Electronic access to healthcare information',
+        'Enabling the right care at the right place',
+        'Efficient, sustainable long-term healthcare, integrated solutions',
+        'Shared clinical electronic record, data exchange & decision making'
+      ]
     },
     {
-    opSelector: "Better information",
-    text: `<li>The use of SNOMED CT in healthcare systems will allow clinicians to record, share and access patient information in a way
-    that retains the semantic fidelity (meaning) of clinical encounters. Researchers and public health planners will also be able to access
-    clinical data collected from a wide range of healthcare systems and perform meaningful queries across the rich clinical detail.</li>`
+      opSelector: "Better information",
+      text: `<li>The use of SNOMED CT in healthcare systems will allow clinicians to record, share and access patient information in a way
+      that retains the semantic fidelity (meaning) of clinical encounters. Researchers and public health planners will also be able to access
+      clinical data collected from a wide range of healthcare systems and perform meaningful queries across the rich clinical detail.</li>`,
+      membersPriorities: [
+        'Electronic access to healthcare information',
+        'Shared clinical electronic record, data exchange & decision making',
+        'Modernisation of systems, secure & seamless electronic access to health information',
+        'Data Protection - regulation/governance of healthcare digitization'
+      ]
     }
 ];
